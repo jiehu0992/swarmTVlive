44db2237874d8aa4b7e0826bf21ff2cc43d26c46
Jem Mackay <jem@swarmtv.org>
2013-02-23 16:11:31 +0000

    Revert "Revert "tries to implement rss in a live context""
    This reverts commit 599d1682b9448b9d62aebd7801178e0ef823763e.

--- private/codeigniter/application/views/rss.php
+++ private/codeigniter/application/views/rss.php
@@ -1,7 +1,8 @@
 <?php  echo '<?xml version="1.0" encoding="' . $encoding . '"?>' . "\n"; ?>  
-<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:admin="http://webns.net/mvcb/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:content="http://purl.org/rss/1.0/modules/content/">  
-    <channel>  
-        <link><?php echo $feed_url; ?>  
+<rss version="2.0">  
+    <channel>
+        <title>Swarm TV RSS feed</title>
+        <link><?php echo $feed_url; ?></link> 
         <description><?php echo $page_description; ?></description>  
         <dc:language><?php echo $page_language; ?></dc:language>  
         <dc:creator><?php echo $creator_email; ?></dc:creator>  
