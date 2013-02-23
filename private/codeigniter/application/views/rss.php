6daeb9f30c28b5b0bb44738e4497cb4be2c527b6
Jem Mackay <jem@swarmtv.org>
2013-02-23 18:30:25 +0000

    validates rss feed a little more

--- private/codeigniter/application/views/rss.php
+++ private/codeigniter/application/views/rss.php
@@ -1,17 +1,20 @@
 <?php  echo '<?xml version="1.0" encoding="' . $encoding . '"?>' . "\n"; ?>  
-<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:admin="http://webns.net/mvcb/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:content="http://purl.org/rss/1.0/modules/content/">  
+<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:admin="http://webns.net/mvcb/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom">  
      <channel>  
+	  <title>Swarm TV Recent Changes</title> 
 	  <link><?php echo $feed_url; ?></link>
 	  <description><?php echo $page_description; ?></description>  
 	  <dc:language><?php echo $page_language; ?></dc:language>  
 	  <dc:creator><?php echo $creator_email; ?></dc:creator>  
 	  <?php foreach($updates->result() as $update): ?>  
 	  <item>  
-	       <title><?php echo $update->summary . ' on page: '. $update->page ?></title>
+	       <title><?php echo ucfirst($update->summary) . ' on page: '. $update->page ?></title>
+	       <guid><?php echo 'Element ' . $update->element_id ?></guid>
 	       <link><?php echo 'http://ucfmediacentre.co.uk/swarmtv/index.php/pages/view/' . $update->page ?></link>   
-	       <pubDate><?php echo gmdate(DATE_RFC822, strtotime($update->pubDate)); ?></pubDate>
+	       <pubDate><?php echo date('r', strtotime($update->pubDate)); ?></pubDate>
 	       <description><![CDATA[ <?php echo $update->jsonArray; ?> ]]></description> 
 	  </item>  
 	  <?php endforeach; ?>
+	  <atom:link href="http://ucfmediacentre.co.uk/swarmtv/index.php/feed" rel="self" type="application/rss+xml" />
      </channel>  
 </rss>  
