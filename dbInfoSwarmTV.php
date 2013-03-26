1566b9fe274832007a2a55fb45b3c7978659d8bb
Jem Mackay <jem@swarmtv.org>
2013-03-26 15:08:56 +0000

    removes forgotten db details!

--- dbInfoSwarmTV.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-//load database variables
-$_SESSION['serverName'] = "79.170.40.38";
-$_SESSION['databaseName'] = "web38-swarmtv";
-$_SESSION['userName'] = "web38-swarmtv";
-$_SESSION['pword'] = "89jhhw7ry";
-?>
