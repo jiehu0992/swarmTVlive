15bcc773037ffcb23b1f0e22bab1d719af652085
Jem Mackay <jem@swarmtv.org>
2013-04-09 09:30:46 +0100

    irons out some bugs due to timeline being blank

--- private/codeigniter/application/controllers/clipPlayer.php
+++ private/codeigniter/application/controllers/clipPlayer.php
@@ -18,9 +18,11 @@ class ClipPlayer extends CI_Controller {
 		$data['altText'] = $record->description;
 		$timelineJSON = $record->timeline;
 		$timeline = json_decode($timelineJSON);
-		$data['in'] = $timeline->in;
-		$data['out'] = $timeline->out;
-		$data['duration'] = $timeline->duration;
+		if ($timeline !== ""){
+		  $data['in'] = $timeline->in;
+		  $data['out'] = $timeline->out;
+		  $data['duration'] = $timeline->duration;
+		}
 		$data['filename']=substr($record->filename, 0 , -4);
 		$data['width']=$record->width;
 		$data['height']=$record->height;
