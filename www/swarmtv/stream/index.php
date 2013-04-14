c1a9c1ac62a8e211ff5c20e91f494fb0ec4b5ec5
Jem Mackay <jem@swarmtv.org>
2013-04-14 22:46:09 +0100

    solves conflicts

--- www/swarmtv/stream/index.php
+++ www/swarmtv/stream/index.php
@@ -101,5 +101,16 @@
     <script src="js/bootstrap-typeahead.js"></script>
 	-->
      <script type="text/javascript" src="js/chat.js"></script>
+        <!-- Google Analytics -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          
+            ga('create', 'UA-39728808-1', 'ucfmediacentre.co.uk');
+            ga('send', 'stream');
+          
+        </script>
   </body>
 </html>
